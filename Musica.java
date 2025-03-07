ebb6cc1b9b1c19dd03581a45f41916d90448ae48
ShofiSo <kelly.rabanales@gmail.com>
2025-03-06 20:51:49 -0600

    Tercer Commit
    Lo requerido en el archivo

--- Musica.java
+++ Musica.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Cancion {
     private String titulo;
     private String artista;
@@ -14,5 +16,46 @@ class Cancion {
 
     public String toString() {
         return "[" + titulo + " - " + artista + " (" + duracion + " min)]";
+    }
+}
+
+class Playlist {
+    private String nombre;
+    private ArrayList <Cancion> canciones;
+
+    public Playlist(String nombre) {
+        this.nombre = nombre;
+        this.canciones = new ArrayList<>();
+    }
+
+    public void agregarCancion(Cancion cancion) {
+        canciones.add(cancion);
+    }
+
+    public void eliminarCancion(String titulo) {
+        canciones.removeIf(c -> c.getTitulo().equalsIgnoreCase(titulo));
+    }
+
+    public Cancion reproducirSiguiente() {
+        return canciones.isEmpty() ? null : canciones.remove(0);
+    }
+
+    public void ordenarPorDuracion() {
+        canciones.sort(Comparator.comparingDouble(Cancion::getDuracion));
+    }
+
+    public void ordenarPorArtista() {
+        canciones.sort(Comparator.comparing(Cancion::getArtista));
+    }
+
+    public void reproducirAleatorio() {
+        Collections.shuffle(canciones);
+    }
+
+    public void mostrarPlaylist() {
+        System.out.println("Playlist: " + nombre);
+        for (Cancion c : canciones) {
+            System.out.println(c);
+        }
     }
 }
